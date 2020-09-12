97037799c45f7507696e48ce591fc61c20f8acf3
Jay Abi-Saad <jayabisaad@gmail.com>
2020-09-12 17:32:12 -0400

    V1 of naive join - still need to make sure it really works and figure out if I'm returning the right vectors

--- join-factory/code/algo-0.c
+++ join-factory/code/algo-0.c
@@ -1,7 +1,47 @@
+#define INITIAL_CAPACITY 3
+
+void push(int *arr, int index, int value, int *size, int *capacity){
+     if(*size > *capacity){
+          int *p = realloc(arr, sizeof(arr) * 2);
+          *capacity = sizeof(arr) * 2;
+     }
+     
+     arr[index] = value;
+     *size = *size + 1;
+}
 
 static void compute_join(int *d1, int len1, int *d2, int len2){
-    // ... join two columns: d1 and d2
-    // write your code here
+    int currD1;
+    int currD2;
+
+    int indexD1 = 0;
+    int sizeD1 = 0;
+    int capacityD1 = INITIAL_CAPACITY;
+    int* v1 = malloc(INITIAL_CAPACITY * sizeof(int));
+   	
+   	int indexD2 = 0;
+    int sizeD2 = 0;
+    int capacityD2 = INITIAL_CAPACITY;
+    int* v2 = malloc(INITIAL_CAPACITY * sizeof(int));
+
+    for(currD1 = 0; currD1 < len1; currD1++) {
+    	for(currD2 = 0; currD2 < len2; currD2++) {
+    		if (d1[currD1] == d2[currD2]) {
+    			push(v1, indexD1, currD1, &sizeD1, &capacityD1);
+    			indexD1++;
+    			push(v2, indexD2, currD2, &sizeD2, &capacityD2);
+    			indexD2++;
+    		}
+    	}
+    }
+
+    for(int p1 = 0; p1 < sizeD2; p1++) {
+		P("[%d] %d\n", v1[p1], d1[v1[p1]]);
+    }
+    P("\n");
+    for(int p2 = 0; p2 < sizeD1; p2++) {
+		P("[%d] %d\n", v2[p2], d2[v2[p2]]);
+    }
 }
 
 static void run_join_a0(V d1, V d2){
@@ -13,4 +53,3 @@ static void run_join_a0(V d1, V d2){
     compute_join(sI(d1), vn(d1), sI(d2), vn(d2));
     time_toc("Join execution time (ms): %g\n", elapsed);
 }
-
