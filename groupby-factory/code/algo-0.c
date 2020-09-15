12eac05d42ee25a2b93ade14fe47d64aa608c783
Jay Abi-Saad <jayabisaad@gmail.com>
2020-09-14 20:35:01 -0400

    WIP of groupby

--- groupby-factory/code/algo-0.c
+++ groupby-factory/code/algo-0.c
@@ -1,6 +1,41 @@
+#define INITIAL_CAPACITY 3
+
+void push(int *arr, int index, int value, int *size, int *capacity){
+     if(*size > *capacity){
+          int *p = realloc(arr, sizeof(arr) * 2);
+          *capacity = sizeof(arr) * 2;
+     }
+     
+     arr[index] = value;
+     *size = *size + 1;
+}
+
+static int isSeen(int value, int *keys, int *size) {
+	for (int i = 0; i < *size; i++) {
+   		if (value == d[i]) {
+   			return i;
+   		}
+   	}
+   	return -1;
+}
+
 static void compute_groupby(int *d, int len){
     // ... groupby the data d
     // write your code here
+	int indexK = 0;
+    int sizeK = 0;
+    int capacityK = INITIAL_CAPACITY;
+    int* keys = malloc(INITIAL_CAPACITY * sizeof(int));
+
+
+   	for (int i = 0; i < len; i++) {
+   		int checkCurr = isSeen(d[i], keys, sizeK);
+   		if (checkCurr == -1) {
+   			push(keys, indexK, &i, &capacity);
+   		} else {
+
+   		}
+   	}
 }
 
 static void run_groupby_a0(V d){
